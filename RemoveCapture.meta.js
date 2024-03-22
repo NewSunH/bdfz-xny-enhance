351b510c7576c8b664d2321905cb74828c9f0240
shaoyujun <huangshangyu2024@i.pkuschool.edu.cn>
2024-03-22 14:38:56 +0800

    三指触控支持

--- RemoveCapture.meta.js
+++ RemoveCapture.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         新能源课程系统调用本地图片
 // @namespace    http://github.com/ShaoYuJun
-// @version      1.0
-// @description  Remove the "capture" attribute from all input elements on xnykc, including dynamically loaded content
+// @version      1.1
+// @description  Remove the "capture" attribute from all input elements on xnykc, including dynamically loaded content and toggle capture mode on triple tap for mobile users
 // @author       Shaoyu
 // @match        *://bdfz.xnykcxt.com:5002/*
 // @grant        none
