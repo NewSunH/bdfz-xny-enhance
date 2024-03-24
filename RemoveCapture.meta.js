0491e086ed563c645719108d4a46d4b1a33ba09d
烧鱼君 <93064707+ShaoYuJun@users.noreply.github.com>
2024-03-24 14:01:50 +0800

    1.3 meta

--- RemoveCapture.meta.js
+++ RemoveCapture.meta.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         新能源课程系统调用本地图片
 // @namespace    http://github.com/ShaoYuJun
-// @version      1.1
-// @description  Remove the "capture" attribute from all input elements on xnykc, including dynamically loaded content and toggle capture mode on triple tap for mobile users
+// @version      1.3
+// @description  重构版！新能源课程网页版支持拍照、上传模式切换！TODO：cookies。特别感谢chatGPT。
 // @author       Shaoyu
 // @match        *://bdfz.xnykcxt.com:5002/*
 // @grant        none
 // @license      GPL-3
 // @downloadURL  https://raw.githubusercontent.com/ShaoYuJun/xny-capture-remove/stable/RemoveCapture.user.js
+// @downloadURL  https://raw.gitmirror.com/ShaoYuJun/xny-capture-remove/stable/RemoveCapture.user.js
 // @updateURL    https://raw.githubusercontent.com/ShaoYuJun/xny-capture-remove/stable/RemoveCapture.meta.js
+// @updateURL    https://raw.gitmirror.com/ShaoYuJun/xny-capture-remove/stable/RemoveCapture.meta.js
 // ==/UserScript==
